4c5bc00b45368a9e08318c488d341205db039976
villabryan88 <villabryan88@gmail.com>
2019-06-13 15:53:07 -0700

    updated link swap, but something is wrong

--- ex00/sortStone.c
+++ ex00/sortStone.c
@@ -6,38 +6,40 @@
 /*   By: bvilla <bvilla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 15:03:23 by bvilla            #+#    #+#             */
-/*   Updated: 2019/06/13 15:22:49 by bvilla           ###   ########.fr       */
+/*   Updated: 2019/06/13 15:50:04 by bvilla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void stone_swap(struct s_stone **a, struct s_stone **b){
-	struct s_stone *tmp;
+void stone_swap(struct s_stone **curr_adr){
+	struct s_stone *curr = *curr_adr;
+	struct s_stone *next = curr->next;
+	struct s_stone *next_next = next->next;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	*curr_adr = next;
+	(*curr_adr)->next = curr;
+	(*curr_adr)->next->next = next_next;
 }
 
 void sortStones(struct s_stone **stone){
 	char swapped = 1;
-	struct s_stone **curr = stone;
-	struct s_stone **next;
+	struct s_stone **curr;
 
-	if (!stone || *stone || (*stone)->next)
+	if (!stone || !*stone || !(*stone)->next)
 		return ;
 	while (swapped)
 	{
+		curr = stone;
 		swapped = 0;
-		for (struct s_stone *i; i->next; i = i->next)
+		for (struct s_stone *i = *stone; i->next; i = i->next)
 		{
-			next = &(i->next);
-			if((*curr)->size > (*next)->size)
+			if((*curr)->size > (i->next)->size)
 			{
-				stone_swap(curr, next);
+				stone_swap(curr);
 				swapped = 1;
 			}
+			curr = &(i->next);
 		}
 	}	
 }
